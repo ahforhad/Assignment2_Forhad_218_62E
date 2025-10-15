5fb53bc8d2a15762c7a7556fde65bbda2f88b698
ahforhad <forhadafjalhussain@gmail.com>
2025-10-15 20:11:33 +0600

    Initial commit

---
+++ 2.dart
@@ -0,0 +1,9 @@
+void main() {
+  String ch = 'B';
+
+  if ('aeiouAEIOU'.contains(ch)) {
+    print("$ch is a Vowel");
+  } else {
+    print("$ch is a Consonant");
+  }
+}
---
+++ 3.dart
@@ -0,0 +1,11 @@
+void main() {
+  int number = 0;
+
+  if (number > 0) {
+    print("$number is Positive");
+  } else if (number < 0) {
+    print("$number is Negative");
+  } else {
+    print("The number is Zero");
+  }
+}
